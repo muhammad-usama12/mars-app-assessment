b96a7a148c2eecac39f0733cdb7a1067eecf5ba7
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-12-15 18:24:35 -0500

    add dynamic form logic to App.js, to control form flow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HealthSafety from "./components/HealthSafety";
@@ -6,16 +6,76 @@ import PersonalInfo from "./components/PersonalInfo";
 import TravelPreferences from "./components/TravelPreferences";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <h1>ORDER OF FORMS</h1>
-      <PersonalInfo />
-      <TravelPreferences />
-      <HealthSafety />
-      <Footer />
-    </div>
-  );
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    dateOfBirth: "",
+    age: "",
+    phoneNum: "",
+    email: "",
+    nationality: "",
+    dietaryChoice: "",
+    allergyChoice: "",
+    healthCondition: "",
+    tierChoice: "",
+    dietaryExtraNeeds: "",
+    allergyExtraNeeds: "",
+    travelExtraNeeds: "",
+    genderChoice: "",
+    healthExtraNeeds: "",
+    healthConditionExtraNeeds: "",
+    specialRequests: "",
+    emergencyFirstName: "",
+    emergencyLastName: "",
+    emergencyPhoneNum: "",
+  });
+
+  const [currentView, setCurrentView] = useState(0);
+
+  const handleCheckboxChange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const finalSubmission = (finalData) => {
+    console.log("Final Form Submitted", finalData);
+  };
+
+  const handleNextStep = (newData, final = false) => {
+    setData((prev) => ({ ...prev, ...newData }));
+
+    if (final) {
+      finalSubmission(newData);
+      return;
+    }
+
+    setCurrentView((prev) => prev + 1);
+  };
+  const handlePreviousStep = (newData) => {
+    setData((prev) => ({ ...prev, ...newData }));
+    setCurrentView((prev) => prev - 1);
+  };
+  console.log("DATA:", data);
+
+  const views = [
+    <PersonalInfo next={handleNextStep} data={data} />,
+    <TravelPreferences
+      next={handleNextStep}
+      back={handlePreviousStep}
+      diet={handleCheckboxChange}
+      allergies={handleCheckboxChange}
+      data={data}
+    />,
+    <HealthSafety
+      next={finalSubmission}
+      back={handlePreviousStep}
+      data={data}
+    />,
+  ];
+  return <div>{views[currentView]}</div>;
 }
 
 export default App;
