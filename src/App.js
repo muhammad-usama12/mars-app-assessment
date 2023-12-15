ec6a524d4064880fb8ec6ac1c1152490ac86458e
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-12-15 10:09:28 -0500

    create header/footer component and compile them to the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import HealthSafety from "./components/HealthSafety";
 import PersonalInfo from "./components/PersonalInfo";
 import TravelPreferences from "./components/TravelPreferences";
@@ -6,10 +8,12 @@ import TravelPreferences from "./components/TravelPreferences";
 function App() {
   return (
     <div>
+      <Header />
       <h1>ORDER OF FORMS</h1>
       <PersonalInfo />
       <TravelPreferences />
       <HealthSafety />
+      <Footer />
     </div>
   );
 }
